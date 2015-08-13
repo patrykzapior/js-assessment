3208321be66dee0c03410d08f6b0b4ebad6b9262
zapiorpatryk <zapior.patryk@gmail.com>
2015-08-13 10:59:58 +0200

    day4

--- impl/day4.js
+++ impl/day4.js
@@ -1,7 +1,85 @@
 (function () {
     'use strict';
-    
+
     // Your implementations here
+    Boxes.prototype = {
+        addBoxes: function(content){
+            if(typeof content === 'string'){
+                this.inBoxes.push(content);
+            }else{
+                this.inBoxes = this.inBoxes.concat(content);
+            }
+        }
+    };
+    Student.prototype = {
+        addMark: function (mark) {
+            if (mark instanceof Array) {
+                for (var i=0;i<=mark.length;i++) {
+                    if (mark[i] >= 2 && mark[i] <= 5 && typeof mark[i] !== 'string') {
+                        this.marks.push(mark[i]);
+                    }
+                }
+                return this.marks;
+            } else {
+                if (mark >= 2 && mark <= 5 && typeof mark !== 'string') {
+                    this.marks.push(mark);
+                }
+            }
+            return this.marks;
+        },
+        average: function(){
+            var avg = 0;
+            for (var i=0;i<this.marks.length;i++){
+                avg += this.marks[i];
+            }
+            return Math.round(avg/this.marks.length *100) /100;
+
+        }
+    };
+
+
+
+    function Boxes(length, width, weight) {
+        this.length = length;
+        this.width = width;
+        this.weight = weight;
+        this.inBoxes = [];
+        this.toStringBoxes = function () {
+            if (this.inBoxes.length > 0) {
+                return 'Inside this boxes is: ' + this.inBoxes.join() + ',';
+            }
+            else {
+                return 'This box is empty';
+            }
+        };
+        this.emptyBoxes = function () {
+            this.inBoxes = '';
+        };
+        this.squareBoxes = function () {
+            return this.width * this.length;
+        };
+        this.transportBoxes = function (car) {
+            if (car.maxSquareTransport >= this.squareBoxes() && car.maxWeightTransport >= this.weight) {
+                return "This car can transport this boxes";
+            } else {
+                return "This car can't transport this boxes";
+            }
+        };
+
+    }
+
+    function Car(maxWeightTransport, maxSquareTransport){
+        this.maxWeightTransport = maxWeightTransport;
+        this.maxSquareTransport = maxSquareTransport;
+    }
+
+    function Student (name, age){
+        this.name = name;
+        this.age = age;
+        this.marks = [];
+    }
+
+
 
     window.day4 = {
         mbox1: new Boxes(10, 10, 10),
@@ -20,8 +98,20 @@
 
         student1: new Student("Dawid Zegar", 23),
         student2: new Student("Jan Kowalski", 77),
-        student3: new Student("Jacek Nowak", 34)
+        student3: new Student("Jacek Nowak", 34),
 
         // Place getBestStudent here
+        getBestStudent: function(students){
+        var x = 0;
+        var best = students[0].average();
+            for (var i=0;i<students.length;i++){
+                if(isNaN(students[i].average())){
+                    return false;
+                }else if(students[i].average() > best){
+                    best = students[i].average;
+                    x = i;
+                }
+            }return students[x];
+        }
     };
 })();
